30bdbf8390b5b41c8be69b17f6ccff7b85901781
elie <elietoure123@gmail.com>
2019-11-07 11:37:29 +0100

    suite fonction

--- index_calcul.py
+++ index_calcul.py
@@ -5,3 +5,10 @@ a = 1
 b = 2
 print(addition(a, b))
 print(soustraction(a, b))
+
+from calcul import addition
+print(addition(5, 6))
+
+import calcul
+calcul.addition(8, 5)
+calcul.soustraction(2, 20)
